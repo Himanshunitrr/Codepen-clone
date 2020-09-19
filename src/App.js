bff89fb1cc04d3aef70097241bd80524ad4cd6ed
Himanshunitrr <himanshumauryacrz@gmail.com>
2020-09-20 04:01:36 +0530

    added editor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Editor from "./components/Editor";
 
 function App() {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
   return (
     <div>
-      
+      <div className="pane top-pane">
+        <Editor 
+        language="xml" 
+        displayName="HTML" 
+        value={html} 
+        onChange={setHtml} />
+        <Editor 
+        language="javascript" 
+        displayName="JS" 
+        value={js} 
+        onChange={setJs} />
+        <Editor 
+        language="css" 
+        displayName="CSS" 
+        value={css} 
+        onChange={setCss} />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+          src=""
+          frameborder="0"
+        />
+      </div>
     </div>
   );
 }
